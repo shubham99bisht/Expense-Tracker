4db73c2c0f728009c8e17a12ac01a39bf00ba33d
Shubham Bisht <shubham@owners-MacBook-Air.local>
2020-03-07 21:27:32 +0530

    added preprocessing

--- BillLabelTool/preprocess.py
+++ BillLabelTool/preprocess.py
@@ -1,20 +1,24 @@
 import os
-from string import ascii_uppercase, ascii_lowercase, digits, punctuation
+from string import ascii_uppercase, ascii_lowercase, digits
 VOCAB = ascii_uppercase +ascii_lowercase +digits + "&$-,.%=/: \t\n"
 
-files = os.listdir("train_data")
+files = os.listdir("data")
 
 for file in files:
     if file[0]==".": continue
-    with open("train_data/"+file, "r") as f:
+    with open("data/"+file, "r") as f:
         text = f.read()
     new_text = ""
     for x in text:
         if x in VOCAB:
             new_text += x
 
+    f = open("original_data/"+file,"w")
+    f.write(new_text)
+    f.close()
+
     new_text = new_text.replace("\n",";\n")
 
-    f = open("preprocessed_data/"+file,"w")
+    f = open("data/"+file,"w")
     f.write(new_text)
     f.close()
